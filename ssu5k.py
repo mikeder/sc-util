9d5bf1ed0adab8d9bb72501e5ba644a91366656e
sqweebking <sqweebking@gmail.com>
2014-10-07 09:16:23 -0400

    Super Awesome Shoutcast Util

--- ssu5k.py
+++ ssu5k.py
@@ -3,7 +3,8 @@
 # Shoutcast Playlist/Transcoder Roller - Mike Eder 2014
 
 # Imports
-import sys, getopt
+import sys
+import getopt
 import os
 import time
 import glob
@@ -22,25 +23,28 @@ plistPath = config.get('paths', 'plistPATH')
 sCount = int(config.get('numbers', 'srmCOUNT'))
 
 # Define all the things
+# Find Server and Transcoder PIDs
 def pids():
  transPid = []
- servPid = ''
+ servPid = []
  transCmd = 'ps x | pgrep sc_trans'
  servCmd = 'ps x | pgrep sc_serv'
  try:
-  serv = subprocess.check_output([servCmd], shell=True).split()
-  print 'Found sc_serv running with PID %d' % (serv)
+  servPid = subprocess.check_output([servCmd], shell=True)
+#  print 'Found sc_serv process: %s' % (servPid)
  except:
-  print 'Server is not running.         (sc_serv)'
+  print '(sc_serv) : Server is not running.'
  try:
   trans = subprocess.check_output([transCmd], shell=True).split()
   for i in range(len(trans)):
    transPid.append(trans[i])
+#   print 'Found sc_trans process: %s' % (trans[i])
    i += 1
  except:
-  print 'Transcoders are not running.   (sc_trans)'
- return (transPid, servPid)
+  print '(sc_trans): Transcoders are not running.'
+ return (servPid, transPid)
 
+# Options that can be used in this script
 def usage():
  print ''
  print 'sc-util accepts the following options:'
@@ -55,6 +59,7 @@ def usage():
  print '(p)rocess:	list process info for sc_trans and sc_serv'
  print ''
 
+# Main decision maker!
 def main(argv):
  if len(sys.argv) < 2:
   print 'No options were given, I need to know what to do..'
@@ -77,44 +82,101 @@ def main(argv):
    generate()
   elif opt in ('-r', '--reload'):
    print 'Reloading playlists'
-   reload()
+   reload(arg)
   elif opt in ('-f', '--flush'):
    print 'Flushing logs with HUP'
    flush()
   elif opt in ('-n', '--next'):
-   print 'Jumping to next song on stream %d' % (arg)
+   print 'Jumping to next song on stream %s' % (arg)
    next(arg)
   elif opt in ('-sh', '--shuffle'):
-   print 'Toggle shuffle on stream %d' % (arg)
+   print 'Toggle shuffle on stream %s' % (arg)
    shuf(arg)
   elif opt in ('-st', '--stop'):
-   print 'Stopping stream %d' % (arg)
+   print 'Stopping stream %s' % (arg)
    stop(arg)
   elif opt in ('-k', '--kill'):
    print 'Killing sc_serv and sc_trans'
    kill()
   elif opt in ('-p', '--process'):
    print 'Checking for running processes..'
-   pids()
+   serv, trans = pids()
+   if serv:
+    print '(sc_serv) : Found running at %s\b' % (serv)
+   else:
+    print '(sc_serv) : Server is not running'
+   if trans:
+    i = 0
+    for i in range(len(trans)):
+     print '(sc_trans): Found running at %s' % (trans[i])
+     i += 1
+   else:
+    print '(sc_trans): Transcoders not running.'
   else:
    print 'Invalid option %s' % (opt)
    usage()
 
+# Start Server and Transcoder(s) if not already running
 def start(sCount):
  i = 1
- while i < sCount:
-  try:
-   transPath = '/home/shoutcast/trans/'
-   servPath = '/home/shoutcast/serv/'
-   cmd1 = '%ssc_trans %sstream%d.conf &' % (transPath, transPath, i)
-   cmd2 = '%ssc_serv %ssc_serv.conf &' % (servPath, servPath)
-   print 'Starting stream %d' % (i)
-   subprocess.call(cmd1, shell=True)
-   i += 1
-  except:
-   print 'Unable to start transcoders'
-   sys.exit(2)
+ serv, trans = pids()
+ if serv == '':
+  print 'Starting sc_serv'
+  servPath = '/home/shoutcast/serv/'
+  cmd2 = 'cd %s && ./sc_serv sc_serv.conf &' % (servPath)
+  subprocess.call(cmd2, shell=True)
+ else:
+  print '(sc_serv) : Server already running.'
+ if len(trans) == 0:
+  while i <= sCount:
+   try:
+    transPath = '/home/shoutcast/trans/'
+# Quiet command to start trans
+    cmd1 = 'cd %s && ./sc_trans stream%d.conf > /dev/null 2> /dev/null &' % (transPath, i)
+# Loud command to start trans
+#    cmd1 = 'cd %s && ./sc_trans stream%d.conf &' % (transPath, i)
+    print 'Starting stream %d' % (i)
+    subprocess.call(cmd1, shell=True)
+    time.sleep(0.1)
+    i += 1
+   except:
+    print 'Unable to start transcoders'
+    sys.exit(2)
+ else:
+  print '(sc_trans): %s Transcoders already running.' % (len(trans))
 
+# Flush - doesnt do anything right now
+def flush():
+ serv, trans = pids()
+ print trans, serv
+
+# Force transcoder to next song
+def next(arg):
+ serv, trans = pids()
+ cmd = 'kill -WINCH %s' % (trans[int(arg)])
+ subprocess.call(cmd, shell=True)
+ print 'Using: ' + cmd
+
+# Reload playlists for all or 1 stream
+def reload(arg):
+ serv, trans = pids()
+ try:
+  if arg:
+   cmd = 'kill -USR1 %s' % (trans[int(arg)])
+   subprocess.call(cmd, shell=True)
+   print 'Using: ' + cmd
+  else:
+   for i in range(len(trans)):
+    cmd = 'kill -USR1 %s' % (trans[i])
+    subprocess.call(cmd, shell=True)
+    print 'Reloading playlist for stream %d' % (i+1)
+    print 'Using: ' + cmd
+    i += 1
+ except:
+  print 'Something went wrong reloading..'
+ 
+# Generate new playlists from source folders
+# - return how many songs added to list and how many total
 def generate():
  i = 0
  srcPath = '/home/music/source/out/'
@@ -122,15 +184,19 @@ def generate():
   try:
    plist = '/home/shoutcast/trans/playlists/stream%d.lst' % (i+1)
    cmd = 'find %s%d -name *.mp3 -print > %s' % (srcPath, i+1, plist)
+   fin = sum(1 for line in open(plist))
    subprocess.call(cmd, shell=True)
-   print 'Generating new playlist for stream%d' % (i+1)
+   print 'Generating new playlist for Stream%d' % (i+1)
+   fout = sum(1 for line in open(plist))
+   print 'Added: %d' % (fout-fin)
    i += 1
   except:
    print 'Error while generating playlists'
 
+# Kill everything
 def kill():
  i = 0
- transPid, servPid = pids()
+ servPid, transPid = pids()
  try:
   while i < len(transPid):
    cmd = 'kill %s' % (transPid[i])
@@ -139,41 +205,13 @@ def kill():
  except subprocess.CalledProcessError:
   print 'Unable to kill processes'
   i += 1
-# def rollPlay():
-# Function to do:
-# - Find new mp3 files in source folders for sCount
-# find /path/to/mp3/directory -type f -name "*.mp3" > playlist.lst
-# - put paths to new mp3 in stream playlist
-# - change symlink for playlist file
-# - send KILL -HUP to PID's to reload list
+
 # - return how many songs added to list and how many total
 
-def rollPlay(sCount):
- i = 1
- while i <= sCount:
-  print '-- Generating new playlist files'
-  
-# def rollTrans(sCount):
-# Function to do:
-# - Check for PID's
-# - if not running start
-# - if running restart
-
-def rollTrans(sCount):
- pids = pids() 
- if not pids:
-  print '-- No running transcoders found'
-  print '-- Attempting to start %d stream transcoders' % (sCount)
-#  try:
-#   i = 1
-#   for i in range(sCount):
-#    startCmd = '/home/music/shoutcast/trans/sc_trans stream%d.conf &' % (i)
- elif len(pids) >= sCount:
-  print '-- Found %d running transcoders..' % (len(pids))
- else:
-  print '-- Expected %d, only found %d running..' % (sCount, len(pids))
-
+start = time.time()
 if __name__ == "__main__":
  main(sys.argv[1:])
 
-print 'Exiting.'
+# print 'Done.'
+end = time.time()
+print 'Finished in %s seconds' % (str(round(end - start, 4)))
